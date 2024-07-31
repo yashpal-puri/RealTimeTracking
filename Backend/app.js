c8941f0857ae803c6cae45979e25e555b86268db
yp8866 <122187543+yp8866@users.noreply.github.com>
2024-07-31 21:08:08 +0530

    oeigin

--- Backend/app.js
+++ Backend/app.js
@@ -6,10 +6,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server,{
     cors: {
-        origin: [
-            "http://localhost:5173",
-            "https://realtimetracking-yashpalpuri.vercel.app/"
-        ],
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
